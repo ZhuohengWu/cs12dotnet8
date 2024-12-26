20bf510cc0508c9c46c80ba8078474b50e69f2fa
Steven Wu <ZhuohengWu@users.noreply.github.com>
2024-12-26 09:48:36 -0500

    learn: chapter 1

---
+++ followcode/Ch1/Chapter01/HelloCS/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, c#!");
+
+string name = typeof(Program).Namespace ?? "None!";
+Console.WriteLine($"Namespace: {name}");
+
+int z;
+
+//throw new Exception();

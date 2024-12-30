71ffc1fc7b2c1f7c9322011b5b917fef2f4517e9
Steven Wu <ZhuohengWu@users.noreply.github.com>
2024-12-30 11:46:36 -0500

    learn: Chapeter 2 and 3 in progress
    - some explore for numbers, fixed vs floating point

--- followcode/Chapter02/Numbers/Program.cs
+++ followcode/Chapter02/Numbers/Program.cs
@@ -47,26 +47,73 @@ Console.WriteLine($"{f1} + {f2} {isEqual3} 0.3");*/
 
 
 
-string rowSeparator = new string('-', count: 104);
+string rowSeparator = new string('-', count: 121);
 WriteLine(rowSeparator);
-WriteLine($"Type    {"Byte(s) of memory",-4} {"Min",32} {"Max",45}");
-WriteLine(rowSeparator);
-WriteLine($"sbyte   {sizeof(sbyte),-4} {sbyte.MinValue,45} {sbyte.MaxValue,45}");
-WriteLine($"byte    {sizeof(byte),-4} {byte.MinValue,45} {byte.MaxValue,45}");
-WriteLine($"short   {sizeof(short),-4} {short.MinValue,45} {short.MaxValue,45}");
-WriteLine($"ushort  {sizeof(ushort),-4} {ushort.MinValue,45} {ushort.MaxValue,45}");
-WriteLine($"int     {sizeof(int),-4} {int.MinValue,45} {int.MaxValue,45}");
-WriteLine($"uint    {sizeof(uint),-4} {uint.MinValue,45} {uint.MaxValue,45}");
-WriteLine($"long    {sizeof(long),-4} {long.MinValue,45} {long.MaxValue,45}");
-WriteLine($"ulong   {sizeof(ulong),-4} {ulong.MinValue,45} {ulong.MaxValue,45}");
-unsafe
-{
-    WriteLine($"Int128  {sizeof(Int128),-4} {Int128.MinValue,45} {Int128.MaxValue,45}");
-    WriteLine($"UInt128 {sizeof(UInt128),-4} {UInt128.MinValue,45} {UInt128.MaxValue,45}");
-    WriteLine($"Half    {sizeof(Half),-4} {Half.MinValue,45} {Half.MaxValue,45}");
-}
-WriteLine($"float   {sizeof(float),-4} {float.MinValue,45} {float.MaxValue,45}");
-WriteLine($"double  {sizeof(double),-4} {double.MinValue,45} {double.MaxValue,45}");
-WriteLine($"decimal {sizeof(decimal),-4} {decimal.MinValue,45} {decimal.MaxValue,45}");
+WriteLine($"{"Type",-8} {"Byte(s) of memory",-4} {"Min",27} {"Max",40} {"Pwr 2 Min",12} {"Pwr 2 Max",12}");
 WriteLine(rowSeparator);
 
+
+PrintRange($"sbyte", sizeof(sbyte), sbyte.MinValue, sbyte.MaxValue);
+PrintRange("byte", sizeof(byte), byte.MinValue, byte.MaxValue);
+PrintRange("short", sizeof(short), short.MinValue, short.MaxValue);
+PrintRange("ushort", sizeof(ushort), ushort.MinValue, ushort.MaxValue);
+PrintRange("int", sizeof(int), int.MinValue, int.MaxValue);
+PrintRange("uint", sizeof(uint), uint.MinValue, uint.MaxValue);
+PrintRange("long", sizeof(long), long.MinValue, long.MaxValue);
+PrintRange("ulong", sizeof(ulong), ulong.MinValue, ulong.MaxValue);
+unsafe
+{
+    PrintRange("Int128", sizeof(Int128), Int128.MinValue, Int128.MaxValue);
+    PrintRange("UInt128", sizeof(UInt128), UInt128.MinValue, UInt128.MaxValue);
+    PrintRange("Half", sizeof(Half), Half.MinValue, Half.MaxValue);
+}
+PrintRange("float", sizeof(float), float.MinValue, float.MaxValue);
+PrintRange("double", sizeof(double), double.MinValue, double.MaxValue);
+PrintRange("decimal", sizeof(decimal), decimal.MinValue, decimal.MaxValue);
+WriteLine(rowSeparator);
+
+
+decimal testDecimal = int.MaxValue;
+testDecimal = 1e11m;
+WriteLine(decimal.GetBits(testDecimal).Length); // four int32 = 96 mantissa
+WriteLine("decimal value = {0}", testDecimal);
+foreach (var bit in decimal.GetBits(testDecimal))
+{
+    Write(Convert.ToString(bit, 2).PadLeft(32, '0') + " "); // Convert to 32-bit binary and pad with leading zeros
+}
+WriteLine();
+
+
+static void PrintRange<T>(string typeName, int size, T min, T max)
+{
+    int bits = size * 8;
+
+    // usigned types: 0 to 2^n - 1;
+    // signed types: -2^(n - 1) to 2^(n - 1) - 1 
+
+    // floating point types: approx powers of 2 (assume half of the bits used for exponent (bits / 2), which is wrong but gives rough idea of the magnitude)
+    //  Type    Bits    sign    Exponent    Mantissa
+    //  Half    16      1       5           10
+    //  float   32      1       8           23
+    //  double  64      1       11          52
+    //  decimal 128     1       5           96
+
+    string powerOf2Min = typeof(T).IsFloatingPointType()
+            ? "~2^-" + (bits / 2)  // approximate limits for floating point types
+            : "-2^" + (typeof(T).IsUnsignedType() ? "0" : $"{bits - 1}");
+
+    string powerOf2Max = typeof(T).IsFloatingPointType()
+            ? "~2^" + (bits / 2)
+            : "2^" + (typeof(T).IsUnsignedType() ? $"{bits} - 1" : $"{bits - 1} - 1");
+
+    Console.WriteLine($"{typeName,-8} {size,-4} {min,40} {max,40} {powerOf2Min,12} {powerOf2Max,12}");
+}
+
+static class TypeExtensions
+{
+    public static bool IsFloatingPointType(this Type type) =>
+        type == typeof(float) || type == typeof(double) || type == typeof(decimal) || type == typeof(Half);
+
+    public static bool IsUnsignedType(this Type type) =>
+        type == typeof(byte) || type == typeof(ushort) || type == typeof(uint) || type == typeof(ulong) || type == typeof(UInt128);
+}
